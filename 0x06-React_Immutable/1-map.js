2a5589c828d6b8cebfa230ed0f4f83fb4817cf91
Lucas Sangura <sanguraowens@gmail.com>
2024-02-22 18:11:32 +0300

    Use export default when possible

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,7 +1,5 @@
 import { Map } from 'immutable';
 
-function getImmutableObject (obj) {
+export default function getImmutableObject (obj) {
   return Map(obj);
 }
-
-export default getImmutableObject;
--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,10 +1,8 @@
 import { Seq } from 'immutable';
 
-function printBestStudents (grades) {
+export default function printBestStudents (grades) {
   Seq(grades).filter((v) => v.score >= 70)
     .map(
       v => `${v.firstName[0].toUpperCase()}${v.firstName.slice(1)} ${v.lastName[0].toUpperCase()}${v.lastName.slice(1)}`)
     .forEach((v) => console.log(v));
 }
-
-export default printBestStudents;

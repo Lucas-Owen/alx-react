b95c0ba58bb9ef736247503901ab258e680c32de
Lucas Sangura <sanguraowens@gmail.com>
2024-03-03 12:19:40 +0300

    Add notificationSelector

--- 0x08-react_redux_reducer_selector/task_5/dashboard/src/selectors/notificationSelector.js
+++ 0x08-react_redux_reducer_selector/task_5/dashboard/src/selectors/notificationSelector.js
@@ -1,5 +1,5 @@
-import { useSelector } from "react-redux";
-
-export const filterTypeSelected = useSelector((state) => state.getIn(["notifications", 'filter']));
-export const getNotifications = useSelector(state => state.getIn(["notifications", "notifications"]));
-export const getUnreadNotifications = useSelector(state => state.getIn(["notifications", "notifications"]).filter(notification => !notification.isRead))
+import { notification } from "../schema/notifications";
+import { Map } from "immutable";
+export const filterTypeSelected = (state) => state.getIn(['filter']);
+export const getNotifications = state => Map(state.getIn(["notifications", "entities", notification.key]));
+export const getUnreadNotifications = state => getNotifications(state).filter(notification => !notification.isRead);

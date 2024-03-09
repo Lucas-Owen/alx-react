25352e9c32966462c0bb64cf2fc7bb5b25cc81da
Lucas Sangura <sanguraowens@gmail.com>
2024-03-09 12:52:14 +0300

    Add task 7

---
+++ 0x09-react_redux_connectors_and_providers/task_7/dashboard/src/actions/courseActionCreators.test.js
@@ -0,0 +1,10 @@
+import { describe, it, expect } from '@jest/globals';
+import { selectCourse, unSelectCourse } from './courseActionCreators';
+import { SELECT_COURSE, UNSELECT_COURSE } from './courseActionTypes';
+
+describe("Test for courseActionCreators", ()=>{
+  it("should return an object with the correct type and index attribute", ()=>{
+    expect(selectCourse(1)).toEqual({type: SELECT_COURSE, index: 1})
+    expect(unSelectCourse(1)).toEqual({type: UNSELECT_COURSE, index: 1})
+  })
+})
---
+++ 0x09-react_redux_connectors_and_providers/task_7/dashboard/src/actions/uiActionCreators.test.js
@@ -0,0 +1,55 @@
+import { describe, it, expect, jest } from '@jest/globals';
+import { displayNotificationDrawer, hideNotificationDrawer, login, loginRequest, logout } from './uiActionCreators';
+import { DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER, LOGIN, LOGIN_FAILURE, LOGIN_SUCCESS, LOGOUT } from './uiActionTypes';
+
+
+import configureStore from 'redux-mock-store';
+import { thunk } from 'redux-thunk';
+
+jest.mock('node-fetch', () => require('fetch-mock-jest').sandbox());
+const fetchMock = require('node-fetch');
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+describe("Test for login", () => {
+  it("should return an object with type LOGIN and user", () => {
+    const email = 'nobody@domain.com';
+    const password = 'password';
+    expect(login(email, password)).toEqual({ type: LOGIN, user: { email, password } });
+  });
+});
+
+describe("Test for logout, displayNotificationsDrawer, hideNotificationsDrawer", () => {
+  it("should return an object with appropriate type", () => {
+    expect(logout()).toEqual({ type: LOGOUT });
+    expect(displayNotificationDrawer()).toEqual({ type: DISPLAY_NOTIFICATION_DRAWER });
+    expect(hideNotificationDrawer()).toEqual({ type: HIDE_NOTIFICATION_DRAWER });
+  });
+});
+
+
+describe("Test for loginRequest action", () => {
+  afterEach(() => {
+    fetchMock.restore();
+  });
+  const email = 'email@domain.com';
+  const password = 'pasWord?';
+  it("should dispatch LOGIN and LOGIN_SUCCESS when request succeeds", () => {
+    const store = mockStore({});
+    fetchMock.get("http://localhost:8654/login-success.json", { status: 200 });
+    return store.dispatch(loginRequest(email, password))
+      .then(() => {
+        const actionsReceived = store.getActions();
+        expect(actionsReceived).toEqual([{ type: LOGIN, user: { email, password } }, { type: LOGIN_SUCCESS }]);
+      });
+  });
+  it("should dispatch LOGIN and LOGIN_FAILURE when request fails", () => {
+    const store = mockStore({});
+    fetchMock.get("http://localhost:8654/login-success.json", { status: 403 });
+    return store.dispatch(loginRequest(email, password))
+      .then(() => {
+        const actionsReceived = store.getActions();
+        expect(actionsReceived).toEqual([{ type: LOGIN, user: { email, password } }, { type: LOGIN_FAILURE }]);
+      });
+  });
+});
